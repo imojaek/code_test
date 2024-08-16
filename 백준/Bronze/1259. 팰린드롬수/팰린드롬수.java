675a488e796bae3c7f1ef51f9e1e22ca636bb453
imojaek <152826980+imojaek@users.noreply.github.com>
2024-08-17 08:53:01 +0900

    [Bronze I] Title: 팰린드롬수, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String str = sc.nextLine();
+            if (str.equals("0")) {
+                break;
+            }
+            int i = 0;
+            boolean flag = false;
+            while (i < str.length() / 2) {
+                if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                    flag = true;
+                    break;
+                }
+                i++;
+            }
+            String result = flag ? "no" : "yes";
+            System.out.println(result);
+        }
+    }
+}
