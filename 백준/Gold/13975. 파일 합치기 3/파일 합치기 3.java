726803f69f5bd3244b3501ff44e853decec805ce
imojaek <152826980+imojaek@users.noreply.github.com>
2024-09-22 00:09:51 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 2452 ms, Memory: 359584 KB -BaekjoonHub

---
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int test = 0; test < T; test++) {
+            int N = Integer.parseInt(br.readLine());
+            PriorityQueue<Long> pq = new PriorityQueue<>();
+
+            String[] input = br.readLine().split(" ");
+            for (int i = 0; i < N; i++) {
+                pq.offer(Long.parseLong(input[i]));
+            }
+
+            Long answer = 0L;
+
+            while (pq.size() > 1) {
+                Long n1 = pq.poll();
+                Long n2 = pq.poll();
+                if (n1 == null || n2 == null)
+                    break;
+
+                answer += n1 + n2;
+                pq.offer(n1 + n2);
+            }
+            System.out.println(answer);
+        }
+
+
+    }
+}
